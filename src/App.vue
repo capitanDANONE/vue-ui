ba6f9d02230ecd6dc866f6c230238f5b710dbd4c
capitanDANONE <128199326+capitanDANONE@users.noreply.github.com>
2025-02-27 20:52:35 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+	AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
